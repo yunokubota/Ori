df6677101ba428c772b65c05a3e0a1a2b661411d
kubota yuno <im.kbxynx@2106gmail.com>
2021-05-25 18:33:10 +0900

    Initial Commit

---
+++ Ori/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ori
+//
+//  Created by 久保田有乃 on 2021/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
